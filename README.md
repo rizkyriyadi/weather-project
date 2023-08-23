3c3a61436fc375f7f1a9d9f90889e65ac774ea6d
Rizky Riyadi <rizkyriady16@gmail.com>
2023-08-24 05:04:26 +0700

    di Weather v1.0.0

--- README.md
+++ README.md
@@ -1,18 +1,27 @@
 
 # Project Title
 
-Create Website that gives current weather info and forecast for the next days
+Dynamic Weather Insights
 
-## Tech Stack
+# Description
+Introducing Personal WeatherTrack, a meticulously crafted weather information hub that I have developed to provide you with a personalized and comprehensive meteorological experience. Seamlessly integrating real-time data and forecasts, this platform is tailored to empower you with the insights you need to make informed daily decisions.
 
-**Client:** HTML, CSS, JavaScript, API
+## Link
+https://rizkyriyadi.github.io/
 
-##
+## Technology 
+- HTML
+- CSS
+- Javascript
+- API's
 
 ## Features
 
 - Current Weather
 - Forcast Weather
+- Today Highlights
 
 ## API
+thanks to : 
 https://openweathermap.org/api
+https://pages.github.com/
--- assets/js/api.js
+++ assets/js/api.js
@@ -1,50 +1,35 @@
-// aa516a8ee270e8c45a076bed2efdf668
+'use strict';
 
-`use strict`;
 const api_key = "aa516a8ee270e8c45a076bed2efdf668";
 
-// Fetch data from
-    
 /**
  * Fetch data from server
- * @param {string} URL API url 
+ * @param {string} URL API url
  * @param {Function} callback callback
  */
-
-
-export const fetchData = function(URL, callback) {
-    fetch(`${URL}&appid=${api_key}`)
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
     .then(res => res.json())
     .then(data => callback(data));
 }
 
 export const url = {
-    currentWeather(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric&appid=aa516a8ee270e8c45a076bed2efdf668`
-    },
-    // forecast
-    forecast(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric&appid=aa516a8ee270e8c45a076bed2efdf668`
-    },
-    // airPolution
-    airPollution(lat, lon) {
-        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}&appid=aa516a8ee270e8c45a076bed2efdf668`
-    },
-    //revers GEO
-    reverseGeo(lat, lon) {
-        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5&appid=aa516a8ee270e8c45a076bed2efdf668`
-    },
-    // geo coding
-    /**
-     * 
-     * @param {string} query Search query (City) 
-    //  */
-    // geo(query) {
-    //     return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=aa516a8ee270e8c45a076bed2efdf668`;
-    // }
-    geo(query) {
-        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=aa516a8ee270e8c45a076bed2efdf668`
-    }
-    
-
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+  },
+  airPollution(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+  },
+  reverseGeo(lat, lon) {
+    return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+  },
+  /**
+   * @param {string} query Search query e.g.: "London", "New York"
+   */
+  geo(query) {
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+  }
 }
