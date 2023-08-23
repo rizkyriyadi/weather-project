54feadb34797aaa5e00ae0962c6f4d41f06c329a
Rizky Riyadi <rizkyriady16@gmail.com>
2023-08-24 00:46:44 +0700

    Fixing Bug

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,6 @@
 // aa516a8ee270e8c45a076bed2efdf668
 
 `use strict`;
-
 const api_key = "aa516a8ee270e8c45a076bed2efdf668";
 
 // Fetch data from
@@ -15,33 +14,36 @@ const api_key = "aa516a8ee270e8c45a076bed2efdf668";
 
 export const fetchData = function(URL, callback) {
     fetch(`${URL}&appid=${api_key}`)
-    .then(res >= res.json())
-    .then(data >= callback(data));
+    .then(res => res.json())
+    .then(data => callback(data));
 }
 
 export const url = {
     currentWeather(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric&appid=aa516a8ee270e8c45a076bed2efdf668`
     },
     // forecast
     forecast(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric&appid=aa516a8ee270e8c45a076bed2efdf668`
     },
     // airPolution
-    forecast(lat, lon) {
-        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}&appid=aa516a8ee270e8c45a076bed2efdf668`
     },
     //revers GEO
     reverseGeo(lat, lon) {
-        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5&appid=aa516a8ee270e8c45a076bed2efdf668`
     },
     // geo coding
     /**
      * 
      * @param {string} query Search query (City) 
-     */
-    geo(lat, lon) {
-        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    //  */
+    // geo(query) {
+    //     return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=aa516a8ee270e8c45a076bed2efdf668`;
+    // }
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=aa516a8ee270e8c45a076bed2efdf668`
     }
     
 
